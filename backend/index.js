e132a634981d682276fa64932529b09a7d20620d
DragonCodes09 <chaudharyrandhirraj2003@gmail.com>
2025-01-26 00:16:20 +0530

    index.html2

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,11 @@ app.use("/api", require("./routes/CartOrderData"));
 app.use("/api", require("./routes/CustOrderData"));
 
 // Serve static files for the frontend
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // Serve the index.html for all routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 // Start the server
